36c6d172ec67f9aa97514d09390d058534bcfd05
Grzegorz Eliszewski <grzegorz.eliszewski@dxc.com>
2018-12-06 15:17:21 +0100

    day 6 - WIP

--- 6/6.rb
+++ 6/6.rb
@@ -7,11 +7,7 @@ def manhattan(p1, p2)
     (p1[0] - p2[0]).abs + (p1[1] - p2[1]).abs
 end
 
-def find_infinite(array)
-    max_x = array.max_by{ |x| x[0] }[0]
-    max_y = array.max_by{ |y| y[1] }[1]
-    p max_x
-    p max_y
+def find_infinite(array, max_x, max_y)
     infinites = Array.new
     array.each do |x|
         if x[0] == 0 or
@@ -23,5 +19,23 @@ def find_infinite(array)
     end
     infinites
 end
+def find_near(points, x, y)
+    distances = []
+    points.each_with_index do |p, i
+end
 
-p input - find_infinite(input)
+def max_region(data)
+    max_x = data.max_by{ |x| x[0] }[0].to_i
+    max_y = data.max_by{ |y| y[1] }[1].to_i
+    grid = {}
+    pt_idx_counts = {}
+    (0..max_x).each do |x|
+        (0..max_y).each do |y|
+            p "#{x}:#{y}"
+        end
+    end
+    # find_infinite(data, max_x, max_y)
+
+end
+
+p max_region(input)
