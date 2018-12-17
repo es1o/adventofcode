0bfde5d6555859fcd0d3ff3ff3c796ae8b0e2f1a
Grzegorz Eliszewski <grzegorz.eliszewski@dxc.com>
2018-12-17 15:28:29 +0100

    Add initial solution for dat 17th.

---
+++ 17/17.rb
@@ -0,0 +1,25 @@
+INPUT = File.readlines('test.txt').map(&:strip)
+# INPUT = File.readlines('input.txt')
+
+p INPUT
+
+p INPUT[0].scan(/=\d?/).first.split('=').first
+map = Array.new
+INPUT.each do |i|
+	x = i.scan(/x=(\d+\.{2}\d+|\d+)/).first.first
+	y = i.scan(/y=(\d+\.{2}\d+|\d+)/).first.first
+	if x =~ /\d+\.{2}\d+/
+		range_x = x.split('..')
+		for j in range_x[0]..range_x[1]
+			map << [j.to_i, y.to_i]
+		end
+	elsif y =~ /\d+\.{2}\d+/
+		range_y = y.split('..')
+		for j in range_y[0]..range_y[1]
+			map << [x.to_i, j.to_i]
+		end
+	end
+	
+end
+
+p map
