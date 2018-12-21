ee818bccbb688c6134a2cdc3bab6f31dfc3d5e18
Grzegorz Eliszewski <grzegorz.eliszewski@dxc.com>
2018-12-21 16:05:06 +0100

    Try to get opcodes for instructions.

--- 16/16.rb
+++ 16/16.rb
@@ -131,6 +131,24 @@ def parse_testcase(test)
 		return false
 	end
 end
+def decode_opcodes(opcodes)
+	run = true
+	result = Hash.new
+	while run
+		# get opcode with 1 number and add to result
+		oo = opcodes.select { |k, v| v.size == 1 }
+		if oo.size > 0
+			oo.each do |k, v|
+				result[k] = v.dup
+				opcodes.each {|a,b| b.delete v.first}
+			end
+		else
+			p opcodes
+			run = false
+		end
+	end
+	result
+end
 
 op_nums = Hash.new
 INPUT.each_with_index do |input, i|
@@ -140,8 +158,8 @@ INPUT.each_with_index do |input, i|
 	end
 end
 
-res.values.map(&:uniq!)
-# p res
-p res.select { |v| v.size > 3 }
-p op_nums
+# PART 1
 p op_nums.count { |k, v| v >= 3 }
+# PART 2
+res.values.map(&:uniq!)
+p decode_opcodes(res)
