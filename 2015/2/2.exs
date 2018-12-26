740350854d7759958b33334c6e84f177b261cbda
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2018-12-26 20:53:55 +0100

    2015 - day02 - part 1

---
+++ 2015/2/2.exs
@@ -0,0 +1,16 @@
+day02 = 0
+
+defmodule Day02 do
+    def area(l, w, h) do
+        2*l*w + 2*w*h + 2*h*l + l*w
+    end
+end
+{:ok, raw } = File.read('input.txt')
+
+input = raw |> String.split("\n", trim: true)
+
+sum_array = Enum.map input, fn z ->
+    [l, w, h] = z |> String.split("x", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sort
+    Day02.area(l, w, h)
+end
+IO.puts "Part1 : #{Enum.sum sum_array}"
