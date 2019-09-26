51c1c603145b7e7a06ae930fb65a8fe8bf4cdcdb
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-09-26 20:28:21 +0200

    2016 - day 5 part 2

--- 2016/5/5.exs
+++ 2016/5/5.exs
@@ -3,16 +3,10 @@ defmodule Day05 do
     case String.length(result) do
       8 -> result
       _ ->
-        # IO.inspect i
         code = Enum.join([input, i])
         hash = :crypto.hash(:md5, code) |> Base.encode16() |> String.graphemes()
         zeros = Enum.take(hash, 5) |> Enum.join
         {:ok, pass} = Enum.fetch(hash, 5)
-        # IO.inspect zeros
-        # IO.inspect code
-        # IO.inspect hash
-        # IO.inspect check(zeros, pass)
-        # IO.inspect check("00000", code)
         solve(input, i + 1, Enum.join([result, check(zeros,pass)]))
     end
   end
@@ -23,6 +17,41 @@ defmodule Day05 do
   defp check(_, _) do
     nil
   end
+  def solve2(input, i, result, n) do
+    case n do
+      8 -> result
+      _ ->
+        code = Enum.join([input, i])
+        hash = :crypto.hash(:md5, code) |> Base.encode16() |> String.graphemes()
+        zeros = Enum.take(hash, 5) |> Enum.join
+        {:ok, pos} = Enum.fetch(hash, 5)
+        {:ok, pass} = Enum.fetch(hash, 6)
+        { code_l, n_pos } = check2(zeros, pass, pos)
+        case n_pos do
+          :nil -> solve2(input, i + 1, result, n)
+          _    -> case elem(result,n_pos) do
+                    0 -> solve2(input, i + 1, :erlang.setelement(n_pos + 1, result, code_l), n + 1)
+                    _ -> solve2(input, i + 1, result, n)
+                  end
+        end
+    end
+  end
+  defp check2("00000", pass, pos) do
+    case Regex.match?(~r/\d/,pos) do
+      true -> 
+        case String.to_integer(pos) do
+          n when n < 8  -> { pass, String.to_integer(pos) }
+          _             -> { :nil, :nil }
+            
+        end
+      _    -> { :nil, :nil }
+    end
+  end
+  defp check2(_, _, _) do
+    { :nil, :nil }
+  end
 end
 
-IO.puts Day05.solve("ojvtpuvg",0,"")
+IO.inspect Day05.solve("ojvtpuvg",0,"")
+result = {0,0,0,0,0,0,0,0}
+IO.inspect Day05.solve2("ojvtpuvg",0,result,0)
