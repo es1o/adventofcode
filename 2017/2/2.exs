b337ce162ccaeb861bc413dec81023d3608011c2
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-10-01 17:56:48 +0200

    2017 - Day 2 - part 2

--- 2017/2/2.exs
+++ 2017/2/2.exs
@@ -1,11 +1,27 @@
 defmodule Day02 do
-    def solve([], sum) do
-        sum
+    def solve([], sum, sum2) do
+        %{:part1 => sum, :part2 => sum2}
     end
 
-    def solve([line|lines], sum) do
+    def solve([line|lines], sum, sum2) do
         new_sum = Enum.max(line) - Enum.min(line)
-        solve(lines, sum + new_sum)
+        new_sum2 = check_divide(line)
+        solve(lines, sum + new_sum, sum2 + new_sum2)
+    end
+    defp check_divide([]) do
+        nil
+    end
+    defp check_divide([line|lines]) do
+        x = Enum.filter(lines, fn x -> rem(x,line) == 0 || rem(line,x) == 0 end)
+        if length(x) > 0 do
+            if line > hd(x) do
+                line/hd(x)
+            else
+                hd(x)/line
+            end
+        else
+            check_divide(lines ++ [line])
+        end
     end
 
 end
@@ -16,4 +32,4 @@ end
 input = raw |> String.split("\n", trim: true) 
             |> Enum.map(fn x -> String.split(x, ~r/\s/, trim: true) end)
             |> Enum.map(fn x -> Enum.map(x, fn z -> String.to_integer(z) end) end)
-IO.inspect Day02.solve(input,0)
+IO.inspect Day02.solve(input,0, 0)
