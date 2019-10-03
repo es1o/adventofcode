9cf805a3a166abe587f292830fc8709266424da2
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-10-03 16:35:10 +0200

    2017 - Day 3 - part 2 - very ugly... :/

--- 2017/3/3.exs
+++ 2017/3/3.exs
@@ -1,18 +1,42 @@
 defmodule Day3 do
-  def solve(step,x,y,final_step,_) when step == final_step do
+  def solve(step,x,y,final_step,_,map) when step == final_step do
     IO.inspect abs(x) + abs(y)
   end
-  def solve(step,x,y,final_step,dir) do
+  def solve(step,x,y,final_step,dir,map) do
     IO.puts "x: #{x}, y: #{y}. step: #{step}"
+    val = case {x,y} do
+      {0,0} ->  1
+      _     ->  nbg(map, x-1,y) +
+                nbg(map, x-1,y-1) +
+                nbg(map, x-1,y+1) +
+                nbg(map, x,y+1) +
+                nbg(map, x,y-1) +
+                nbg(map, x+1,y+1) +
+                nbg(map, x+1,y) +
+                nbg(map, x+1,y-1)
+    end
+    if val > final_step do
+      IO.inspect "DAY2: #{val}"
+    end
+
+    new_map = Map.merge(map, %{"#{x}:#{y}" => val})
     case dir do
-      :r -> solve(step + 1, x + 1, y, final_step, new_dir(x + 1,y,dir))
-      :l -> solve(step + 1, x - 1, y, final_step, new_dir(x - 1,y,dir))
-      :u -> solve(step + 1, x, y + 1, final_step, new_dir(x,y + 1,dir))
-      :d -> solve(step + 1, x, y - 1, final_step, new_dir(x,y - 1,dir))
+      :r -> solve(step + 1, x + 1, y, final_step, new_dir(x + 1,y,dir),new_map)
+      :l -> solve(step + 1, x - 1, y, final_step, new_dir(x - 1,y,dir),new_map)
+      :u -> solve(step + 1, x, y + 1, final_step, new_dir(x,y + 1,dir),new_map)
+      :d -> solve(step + 1, x, y - 1, final_step, new_dir(x,y - 1,dir),new_map)
       _  -> nil
         
     end
     
+  end
+  defp nbg(map,x,y) do
+    n = Map.get(map, "#{x}:#{y}")
+    if n do
+      n
+    else
+      0
+    end
   end
   defp new_dir(x,y,dir) do
     cond do
@@ -27,4 +51,5 @@ defmodule Day3 do
   end
 end
 
-Day3.solve(1,0,0,265149,:r)
+IO.inspect Day3.solve(1,0,0,265149,:r, %{})
+# IO.inspect Day3.solve(1,0,0,12,:r, %{})
