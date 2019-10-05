cbc669a21ed9dc09f3ce139888de816b41788c03
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-10-05 15:21:33 +0200

    2017 - Day4 - part2

--- 2017/4/4.exs
+++ 2017/4/4.exs
@@ -1,16 +1,32 @@
 defmodule Day04 do
-    def solve([], valid) do
-        valid
+    def solve([], valid, valid2,part) do
+        case part do
+            :part1 -> valid
+            :part2 -> valid2
+        end
     end
 
-    def solve([line|lines], valid) do
+    def solve([line|lines], valid, valid2, part) do
         list = line |> String.split(" ", trim: true)
-        uniq_size = list |> Enum.uniq |> Enum.count
-        line_size = list |> Enum.count
-        if uniq_size != line_size do 
-            solve(lines, valid)
-        else
-            solve(lines, valid + 1)
+        case part do
+            :part1 ->
+                uniq_size = list |> Enum.uniq |> Enum.count
+                line_size = list |> Enum.count
+                if uniq_size != line_size do 
+                    solve(lines, valid, valid2,part)
+                else
+                    solve(lines, valid + 1,valid2,part)
+                end
+
+            :part2 ->
+                chars_list = list |> Enum.map(fn x -> String.graphemes(x) end) |> Enum.map(fn z -> Enum.sort(z) end)
+                uniq_chars_size = chars_list |> Enum.uniq |> Enum.count
+                chars_size = chars_list |> Enum.count
+                if uniq_chars_size != chars_size do 
+                    solve(lines, valid, valid2,part)
+                else
+                    solve(lines, valid,valid2 + 1,part)
+                end
         end
     end
 
@@ -21,4 +37,5 @@ end
 
 input = raw |> String.split("\n", trim: true) 
 
-IO.inspect "Part1: #{Day04.solve(input,0)}"
+IO.inspect "Part1: #{Day04.solve(input,0,0,:part1)}"
+IO.inspect "Part2: #{Day04.solve(input,0,0,:part2)}"
