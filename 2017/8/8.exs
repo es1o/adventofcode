fbddf51c67b415173c45ee1a14e0b782ea589a5f
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-10-10 09:46:15 +0200

    2017 - Day 8 - part 2

--- 2017/8/8.exs
+++ 2017/8/8.exs
@@ -1,23 +1,37 @@
 defmodule Day08 do
-    def solve([], reg) do
+    def solve([], reg, max) do
         {:ok, part1} = Map.values(reg) |> Enum.sort(&(&1 >= &2))|> Enum.fetch(0)
-        part1
+        {:ok, part2} = Map.values(max) |> Enum.sort(&(&1 >= &2))|> Enum.fetch(0)
+        {part1, part2}
 
     end
-    def solve([cmd|cmds], reg) do
-        new_reg = run_cmd(cmd, reg)
-        solve(cmds,new_reg)
+    def solve([cmd|cmds], reg, max) do
+        {new_reg, new_max} = run_cmd(cmd, reg, max)
+        solve(cmds,new_reg, new_max)
         
     end
 
-    defp run_cmd(cmd, reg) do
+    defp run_cmd(cmd, reg, max) do
         [regA, command, x, "if", regB, cnd, y] = cmd |> String.split("\s", trim: true)
         new_reg = Map.put_new(reg, regB, 0) |> Map.put_new(regA, 0)
         if check_cond(regB, cnd, y, new_reg) do
             final_reg = compute(regA, command, x, new_reg)
-            final_reg
+            final_max = check_max(regA, final_reg, max)
+            {final_reg, final_max}
         else
-            new_reg
+            {new_reg, max}
+        end
+        
+    end
+
+    defp check_max(regA, reg, max) do
+        new_max = Map.put_new(max, regA, 0)
+
+        reg_val = Map.get(reg, regA)
+        if reg_val > Map.get(new_max, regA) do
+            Map.update(new_max, regA, 0, fn _ -> reg_val end)
+        else
+            new_max
         end
         
     end
@@ -50,4 +64,6 @@ end
 
 input = raw |> String.split("\n", trim: true) 
 
-IO.inspect "part1 #{Day08.solve(input,%{})}"
+{part1, part2} = Day08.solve(input,%{},%{})
+IO.inspect "part1 #{part1}"
+IO.inspect "part2 #{part2}"
