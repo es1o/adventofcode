14d32273b9c66af7bc43f2dcb7dfd135876bc3c7
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-12-02 19:20:12 +0100

    2015 - Day 3 - part 2 - works for testing data only... :/

--- 2015/3/3.exs
+++ 2015/3/3.exs
@@ -19,6 +19,40 @@ defmodule Day03 do
         end
     end
 
+    def calculate([], map, houses, _, _, _, _, _) do
+        houses
+    end
+
+    def calculate(data, map, houses, sx, sy, rx, ry, robo) do
+        [ head | tail ] = data
+        {x, y} = case robo do
+            true -> {rx, ry}
+            false -> {sx, sy}
+        end
+        [new_x, new_y ] = case head do
+            "^" -> [ x, y + 1 ]
+            "v" -> [ x, y - 1 ]
+            ">" -> [ x + 1, y ]
+            "<" -> [ x - 1, y ]
+            _ -> [x, y]
+        end
+        # IO.inspect {x,y}
+        if Enum.member? map, {new_x, new_y} do
+            # IO.puts "TRU"
+            case robo do
+                true -> calculate(tail, map, houses, sx, sy, new_x, new_y, !robo)
+                false -> calculate(tail, map, houses, new_x, new_y, rx, ry, !robo)
+            end
+        else
+            # IO.puts "FALSE"
+            case robo do
+                true -> calculate(tail, map ++ [{new_x,new_y}], houses + 1, sx, sy, new_x, new_y, !robo)
+                false -> calculate(tail, map ++ [{new_x,new_y}], houses + 1, new_x, new_y, rx, ry, !robo)
+            end
+        end
+    end
+
+
 
 end
 
@@ -27,6 +61,8 @@ end
 
 input = raw |> String.split("", trim: true)
 
-part1 = Day03.calculate(input, [], 0, 0, 0)
+# part1 = Day03.calculate(input, [], 0, 0, 0)
+part2 = Day03.calculate(input, [], 1, 0, 0, 0, 0, false)
 
-IO.puts "Part1: #{part1}"
+# IO.puts "Part1: #{part1}"
+IO.puts "Part2: #{part2}"
