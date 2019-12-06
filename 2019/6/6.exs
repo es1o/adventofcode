7802cdc1843cb1aa44b9646156a6dd038923e743
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2019-12-06 11:54:45 +0100

    2019 - Day 6 - part 2

--- 2019/6/6.exs
+++ 2019/6/6.exs
@@ -1,7 +1,12 @@
 defmodule Day6 do
   def solve([], orbits, map) do
     keys = Map.keys(map)
-    iterate(keys, map, orbits)
+    p1 = iterate(keys, map, orbits)
+
+    {_, santa_list} = check_orbits(map, "SAN", orbits, [])
+    {_, you_list} = check_orbits(map, "YOU", orbits, [])
+    p2 = length(santa_list -- you_list) + length(you_list -- santa_list)
+    { p1, p2}
   end
   def solve(input, orbits, map) do
     [h|t] = input
@@ -9,19 +14,20 @@ defmodule Day6 do
     new_map = Map.put(map, o2, o1)
     solve(t, orbits, new_map)
   end
-  defp iterate([], map, orbits) do
+  defp iterate([], _, orbits) do
     orbits
   end
   defp iterate(keys, map, orbits) do
     [h|t] = keys
-    new_orbits = check_orbits(map, h, orbits)
+    {new_orbits, _} = check_orbits(map, h, orbits,[])
     iterate(t, map, new_orbits)
   end
-  defp check_orbits(map, p, orbits) do
+  defp check_orbits(map, p, orbits, list) do
     if Map.has_key?(map, p) do
-      check_orbits(map, map[p], orbits + 1)
+      check_orbits(map, map[p], orbits + 1, list ++ [map[p]])
     else
-      orbits
+      # IO.inspect list
+      { orbits, list }
     end
   end
 end
@@ -30,4 +36,6 @@ end
 # {:ok, raw } = File.read('test.txt')
 
 input = raw |> String.split("\n", trim: true)
-IO.inspect "part1: #{Day6.solve(input, 0, %{})}"
+{p1, p2} = Day6.solve(input, 0, %{})
+IO.inspect "part1: #{p1}"
+IO.inspect "part2: #{p2}"
