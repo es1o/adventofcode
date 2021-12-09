343b1b5a2a223a7b7c5080e15dc498fddcea2590
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2021-12-09 10:24:49 +0100

    2021 - Day 9 - partial part 2

--- 2021/9/9.py
+++ 2021/9/9.py
@@ -1,7 +1,23 @@
-# f = open("test.txt")
-f = open("input.txt")
+f = open("test.txt")
+# f = open("input.txt")
 
 arr = []
+basins = []
+
+def get_low(arr, x, y):
+  if y > 0:
+    if arr[x][y-1] > arr[x][y] < 9:
+      return 1 + get_low(arr, x, y -1) 
+  elif y < len(arr[x]) - 1:
+    if arr[x][y+1] > arr[x][y] < 9:
+      return 1 + get_low(arr, x, y +1) 
+  if x > 0:
+    if arr[x-1][y] > arr[x][y] < 9:
+      return 1 + get_low(arr, x - 1, y) 
+  elif x < len(arr) - 1:
+    if arr[x+1][y] > arr[x][y] < 9:
+      return 1 + get_low(arr, x+1, y -1) 
+  return 0
 
 def get_adjacent(arr, x, y):
   result = []
@@ -23,6 +39,45 @@ def get_adjacent(arr, x, y):
   # print(result)
   if min(result) > arr[x][y]:
     # print("ESIO")
+    i = x
+    basin = 0
+    basin = get_low(arr, x, y)
+    # while i > 0:
+    #   if arr[i-1][y] == 9:
+    #     break
+    #   elif arr[i-1][y] > arr[i][y]:
+    #     basin = basin + 1  
+    #     i = i - 1
+    #   else:
+    #     break
+    # i = x
+    # while i < len(arr) - 2:
+    #   if arr[i+1][y] == 9:
+    #     break
+    #   elif arr[i+1][y] > arr[i][y]:
+    #     basin = basin + 1  
+    #     i = i + 1
+    #   else:
+    #     break
+    # i = y
+    # while i > 0:
+    #   if arr[x][i-1] == 9:
+    #     break
+    #   elif arr[x][i-1] > arr[x][y]:
+    #     basin = basin + 1  
+    #     i = i - 1
+    #   else:
+    #     break
+    # while i < len(arr[x]) - 2:
+    #   if arr[x][i+1] == 9:
+    #     break
+    #   elif arr[x][i+1] > arr[x][i]:
+    #     basin = basin + 1  
+    #     i = i + 1
+    #   else:
+    #     break
+    print("min", arr[x][y])
+    print("basin", basin)
     return arr[x][y] + 1
   else:
     return 0
